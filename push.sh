fbd7c3d907477792f78908c0ea634de57e370f03
Max Wernz <mawernz@outlook.de>
2023-05-08 22:43:09 +0200

    push commit

---
+++ push.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+PWD=$(basename "$PWD")
+
+if git ls-files --others --exclude-standard | grep -q .; then
+    # If there are untracked files, use git add to add them to the index
+    git add .
+    echo "Untracked files added to git index."
+fi
+
+git commit -m 'push commit' -a
+
+git pull
+
+git push
+
+cd ..
+
+
+if [ -d "/Volumes/T7" ]; then
+    rsync -av --exclude='.git*' "$PWD"/ps_bericht /Volumes/T7/studium/praxissemester
+fi
+
+rsync -av --exclude='.git*' "$PWD"/ps_bericht ~/Library/Mobile\ Documents/com\~apple\~CloudDocs/Studium/Praxissemester/PS_Bericht/
+
