e41a444ed21d5e1cf49b63bf8cb70ba03519ebb7
Max Wernz <mawernz@outlook.de>
2023-05-18 14:08:54 +0200

    push commit

--- push.sh
+++ push.sh
@@ -18,8 +18,10 @@ cd ..
 
 
 if [ -d "/Volumes/T7" ]; then
+    rm -r /Volumes/T7/studium/praxissemester
     rsync -av --exclude='.git*' "$PWD"/ps_bericht /Volumes/T7/studium/praxissemester
 fi
 
+rm -r ~/Library/Mobile\ Documents/com\~apple\~CloudDocs/Studium/Praxissemester/PS_Bericht/
 rsync -av --exclude='.git*' "$PWD"/ps_bericht ~/Library/Mobile\ Documents/com\~apple\~CloudDocs/Studium/Praxissemester/PS_Bericht/
 
